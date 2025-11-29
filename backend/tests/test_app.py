40ec6adeba73e2bf69a9819cab7860fb1a0b9d08
DiegoCam55 <cameldiego55@gmail.com>
2025-11-29 01:26:22 -0500

    Add files via upload

---
+++ backend/tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    res = client.get('/')
+    assert res.status_code == 200
+    assert b"Backend Flask funcionando correctamente" in res.data
+
